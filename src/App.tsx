5f6bb3c835359d61f57aed1dc63f38ac84a7b1bb
bitecroissant <bitecroissant@hotmail.com>
2025-04-12 22:03:54 +0800

    🪂 手动安装 tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <>
-      <h1 className="fonts-jinbuti">你好</h1>
+      <h1 className="fonts-jinbuti text-3xl font-bold underline">你好</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button b-1 onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
