6f6d6cb442122ef6e28b64cc25ac3483e24737fa
bitecroissant <bitecroissant@hotmail.com>
2025-04-19 17:46:30 +0800

    👩‍👩‍👦‍👦 没有更多数据时隐藏加载更多按钮

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -48,65 +48,72 @@ export const EventsPage: React.FC = () => {
     return <PageLoading />
   }
 
+  if (!eventsData) {
+    return (
+      <div className="pt-16 flex flex-col items-center justify-center">
+        <CalendarDays className="w-[64px] h-[64px] text-gray-700" />
+        <h1 className="text-m mt-4 text-gray-700">您还没有创建任何事件</h1>
+        <Button onClick={onClickNewEvent} className="mt-4 rounded-full">
+          创建第一个事件
+        </Button>
+      </div>
+    )
+  }
+
+  const lastEventsData = eventsData[eventsData.length - 1]
+  const { page_no, per_page, total } = lastEventsData.pager
+  const hasMore = (page_no - 1) * per_page + lastEventsData.resources.length < total
+
   return (
     <div className="pb-96 ">
       <EventsPageHeader title="我的事件" subtitle="管理和记录您重要的事件" />
-      {eventsData
-        ? (
-            <>
-              <ol className="pt-4 space-y-4">
-                {eventsData.map(({ resources }) => {
-                  return resources.map((item) => {
-                    return (
-                      <li className="px-4" key={item.id}>
-                        <Card className={cs('relative flex p-4 space-x-4')}>
-                          <Badge className="rounded-full h-[52px] w-[52px] p-0 flex items-center justify-center bg-orange-600">
-                            <Icon className="text-white" name={item.icon_name || 'robot'} />
-                          </Badge>
-                          <div className="flex-1">
-                            <p>
-                              {item.emoji && <span>{item.emoji}</span>}
-                              <span className="fonts-jinbuti font-bold">{item.name}</span>
-                            </p>
-                            <div className="flex items-center justify-between text-s space-x-1 text-gray-500">
-                              <p className="flex items-center just"><History className="h-[18px] w-[18px] mr-1" /> 过了大约 239999 天</p>
-                              <p>2025-12-31</p>
-                            </div>
-                          </div>
+      <ol className="pt-4 space-y-4">
+        {eventsData.map(({ resources }) => {
+          return resources.map((item) => {
+            return (
+              <li className="px-4" key={item.id}>
+                <Card className={cs('relative flex p-4 space-x-4')}>
+                  <Badge className="rounded-full h-[52px] w-[52px] p-0 flex items-center justify-center bg-orange-600">
+                    <Icon className="text-white" name={item.icon_name || 'robot'} />
+                  </Badge>
+                  <div className="flex-1">
+                    <p>
+                      {item.emoji && <span>{item.emoji}</span>}
+                      <span className="fonts-jinbuti font-bold">{item.name}</span>
+                    </p>
+                    <div className="flex items-center justify-between text-s space-x-1 text-gray-500">
+                      <p className="flex items-center just"><History className="h-[18px] w-[18px] mr-1" /> 过了大约 239999 天</p>
+                      <p>2025-12-31</p>
+                    </div>
+                  </div>
 
-                          <div className="absolute right-0 top-0 flex space-x-2 p-2">
-                            {item.is_pin > 0 && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-orange-600"><Pin className="w-[14px] h-[14px] text-white" /></Badge>}
-                            {item.is_loop > 0 && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-blue-600"><RefreshCw className="w-[14px] h-[14px] text-white" /></Badge>}
-                            {!(item.is_active > 0) && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-red-600"><Ban className="w-[14px] h-[14px] text-white" /></Badge>}
-                          </div>
-                        </Card>
-                      </li>
-                    )
-                  })
-                })}
-              </ol>
-              <div className="flex justify-center mt-4">
-                <Button
-                  className="shadow-lg rounded-full"
-                  variant="outline"
-                  disabled={eventsValidating || eventsLoading}
-                  onClick={onLoadMore}
-                >
-                  {(eventsLoading || eventsValidating) ? <Loader className="animate-spin" /> : <ChevronDown />}
-                  {(eventsLoading || eventsValidating) ? '加载中...' : '加载更多'}
-                </Button>
-              </div>
-            </>
-          )
-        : (
-            <div className="pt-16 flex flex-col items-center justify-center">
-              <CalendarDays className="w-[64px] h-[64px] text-gray-700" />
-              <h1 className="text-m mt-4 text-gray-700">您还没有创建任何事件</h1>
-              <Button onClick={onClickNewEvent} className="mt-4 rounded-full">
-                创建第一个事件
+                  <div className="absolute right-0 top-0 flex space-x-2 p-2">
+                    {item.is_pin > 0 && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-orange-600"><Pin className="w-[14px] h-[14px] text-white" /></Badge>}
+                    {item.is_loop > 0 && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-blue-600"><RefreshCw className="w-[14px] h-[14px] text-white" /></Badge>}
+                    {!(item.is_active > 0) && <Badge className="rounded-full h-[24px] w-[24px] p-0 flex items-center justify-center bg-red-600"><Ban className="w-[14px] h-[14px] text-white" /></Badge>}
+                  </div>
+                </Card>
+              </li>
+            )
+          })
+        })}
+      </ol>
+      { hasMore
+        ? (
+            <div className="flex justify-center mt-4">
+              <Button
+                className="shadow-lg rounded-full"
+                variant="outline"
+                disabled={eventsValidating || eventsLoading}
+                onClick={onLoadMore}
+              >
+                {(eventsLoading || eventsValidating) ? <Loader className="animate-spin" /> : <ChevronDown />}
+                {(eventsLoading || eventsValidating) ? '加载中...' : '加载更多'}
               </Button>
             </div>
-          )}
+          )
+        : (<div className="flex justify-center mt-4">没有更多数据了...</div>)}
+
       <SiteNav />
     </div>
   )
