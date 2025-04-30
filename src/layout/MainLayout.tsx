b117b4b2e4bd4297bbce8a11ac899b90b013a5c1
bitecroissant <bitecroissant@hotmail.com>
2025-04-30 12:52:04 +0800

    🦅 调试x2

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,12 +2,12 @@ import { PageLoading } from '@/components/PageLoading'
 import { Toaster } from '@/components/ui/toaster'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useEffect } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
 
 export const MainLayout: React.FC = () => {
-  const nav = useNavigate()
-  const location = useLocation()
-  const { pathname } = location
+  // const nav = useNavigate()
+  // const location = useLocation()
+  // const { pathname } = location
 
   const { isLoading, isAuthenticated, getAccessTokenSilently } = useAuth0()
   const setJwt = async () => {
@@ -19,16 +19,16 @@ export const MainLayout: React.FC = () => {
   }
 
   useEffect(() => {
-    if ((pathname === '/' || pathname === '/session') && isAuthenticated) {
-      // eslint-disable-next-line no-alert
-      window.alert(`${isLoading} && ${isAuthenticated}`)
-      nav('/events')
-    }
-    if (!isLoading && !isAuthenticated) {
-      // eslint-disable-next-line no-alert
-      window.alert(`${isLoading} && ${isAuthenticated}`)
-      nav('/session')
-    }
+    // if ((pathname === '/' || pathname === '/session') && isAuthenticated) {
+    //   // eslint-disable-next-line no-alert
+    //   window.alert(`${isLoading} && ${isAuthenticated}`)
+    //   nav('/events')
+    // }
+    // if (!isLoading && !isAuthenticated) {
+    //   // eslint-disable-next-line no-alert
+    //   window.alert(`${isLoading} && ${isAuthenticated}`)
+    //   nav('/session')
+    // }
   }, [isLoading, isAuthenticated])
 
   useEffect(() => {
@@ -43,7 +43,8 @@ export const MainLayout: React.FC = () => {
 
   return (
     <main className=" bg-gradient-to-r from-[#080f18] via-[#030a15] to-[#080f18] relative min-h-screen list-none ">
-      <Outlet />
+      <p className="text-white text-xl">{isLoading ? '加载中' : '加载完了'}{isAuthenticated ? '授权' : '未授权'}</p>
+      {/* <Outlet /> */}
       <Toaster />
     </main>
   )
